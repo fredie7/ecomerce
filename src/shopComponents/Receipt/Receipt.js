9d86f6eb6a3dbb7b6b18f8ac2fee9e65fe1c1fc1
fredie <lexfredeick3@gmail.com>
2021-06-09 00:43:00 +0100

    receipt

---
+++ src/shopComponents/Receipt/Receipt.js
@@ -0,0 +1,61 @@
+import React, {useContext} from 'react'
+import classes from './Receipt.module.css'
+import {ProductContext} from '../../context/ProductContext'
+
+const Receipt = () => {
+    const { cart, cartTotalDetails } = useContext(ProductContext)
+    const{ cartTotals } = cartTotalDetails
+
+    const renderReceipt = cart.map(item => {
+        const { title,price,total,count } = item
+        return (
+            <div className={classes.ReceiptDetail}>
+                <p className={classes.Title}>{title}</p>
+                <p className={classes.Price}><span>$</span>{price}</p>
+                <p className={classes.Quantity}>{count}</p>
+                <p className={classes.Amount}><span>$</span>{total}</p>
+            </div> 
+        )
+    })
+    return (
+        <>
+            <div className={classes.Container}>
+                <div className={classes.ReceiptContainer}>
+                    <div className={classes.ReceiptDetail}>
+                        {/* <p className={classes.Title}>Product(s)</p>
+                        <p className={classes.Price}>Price</p>
+                        <p className={classes.Quantity}>Quantity</p>
+                        <p className={classes.Amount}>Amount</p> */}
+                    </div>
+                    <div className={classes.ReceiptData}>
+                       {renderReceipt}
+                        {/* <div className={classes.ReceiptDetail}>
+                            <p className={classes.Title}>Lorem ipsuw</p>
+                            <p className={classes.Price}><span>$</span>10</p>
+                            <p className={classes.Quantity}>10</p>
+                            <p className={classes.Amount}><span>$</span>10</p>
+                        </div>
+                        <div className={classes.ReceiptDetail}>
+                            <p className={classes.Title}>Lorem ipsuw</p>
+                            <p className={classes.Price}><span>$</span>10</p>
+                            <p className={classes.Quantity}>10</p>
+                            <p className={classes.Amount}><span>$</span>10</p>
+                        </div>
+                        <div className={classes.ReceiptDetail}>
+                            <p className={classes.Title}>Lorem ipsuw</p>
+                            <p className={classes.Price}><span>$</span>10</p>
+                            <p className={classes.Quantity}>10</p>
+                            <p className={classes.Amount}><span>$</span>10</p>
+                        </div> */}
+                    </div>
+                    <div className={classes.Print}>
+                        <h3>print receipt</h3>
+                        <h3>total: {cartTotals.toFixed(2)}</h3>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Receipt
